cfaa0f5095047b456767f8edee27d12473321913
justapig9020 <justapig9020@gmail.com>
2024-09-16 12:51:55 +0800

    add script for build spann index

--- Script_AE/build_spann_index.sh
+++ Script_AE/build_spann_index.sh
@@ -6,6 +6,7 @@ datatype="uint8"
 dimension=128
 base_size=100000000
 query_size=10000
+posting_page_limit=1
 
 current_path=$(pwd)
 # Path to the files, change if necessary
@@ -14,8 +15,9 @@ query_file="${current_path}/dataset/sift_data/query.public.10K.u8bin"
 binary_path="${current_path}/../Release"
 trace_and_truth_path="${current_path}/dataset/sift_data"
 ini_file="${current_path}/iniFile/build_spann.ini"
-index_path="${current_path}/dataset/spann_index"
+index_path="${current_path}/dataset/spann_index_page${posting_page_limit}"
 tmp_path="${current_path}/dataset/tmp"
+log_file="${index_path}/build_index.log"
 
 if [[ ${datatype} == "uint8" ]]; then
 	value_type="UInt8"
@@ -49,6 +51,7 @@ config=(
 	"16 WarmupSize=${query_size}"
 	"18 TruthPath=${trace_and_truth_path}/"
 	"23 IndexDirectory=${index_path}"
+  "54 PostingPageLimit=${posting_page_limit}"
 	"56 TmpDir=${tmp_path}"
 )
 
@@ -57,4 +60,7 @@ for item in "${config[@]}"; do
 	replace "${line}" "${text}" "${ini_file}"
 done
 
-time "${binary_path}/ssdserving" "${ini_file}" | tee build_index.log
+
+echo "Log file: ${log_file}"
+time "${binary_path}/ssdserving" "${ini_file}" | tee build_spann_index.log
+mv build_spann_index.log "${log_file}"
